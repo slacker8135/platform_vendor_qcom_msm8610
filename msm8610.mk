241bd7f147edf4fba447f007ff36055d3fe342b7
Bhargav Gurappadi <bhargavg1@codeaurora.org>
2014-01-16 14:08:44 -0800

    LE devices arent supported
    Link against the right main.conf file
    
    Change-Id: I8b81a664e9b9a606a3fa76a52c0bc14955da438b
    
    CRs-Fixed: 439030

--- msm8610.mk
+++ msm8610.mk
@@ -36,7 +36,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth configuration files
 PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
+    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf \
     system/bluetooth/data/audio.conf:system/etc/bluetooth/audio.conf
 
 #fstab.qcom
